7c44ac7c26cf2f11f2a025bd91c469053c12d13a
thiagocardoso1 <cardosot204@gmail.com>
2024-12-13 20:05:11 -0300

    add section sobre create about css and modify styles

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,3 +7,21 @@ icon.addEventListener("click", () => {
     htmlElement.getAttribute("data-theme") === "dark" ? htmlElement.setAttribute("data-theme", "light") : htmlElement.setAttribute("data-theme", "dark");
     
 })
+
+function showDetails(accordionItem) {
+    accordionItem.addEventListener("click", () => {
+        accordionItem.classList.toggle("active");
+    });
+};
+
+const accordionItems = document.querySelectorAll(".accordion__item");
+accordionItems.forEach((accordionItem) => {showDetails(accordionItem)});
+
+const menuLinks = document.querySelectorAll(".menu__item");
+
+menuLinks.forEach((item) => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach((i) => {i.classList.remove("active")});
+        item.classList.add("active");
+    });
+});
